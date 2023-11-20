2ded9f94038f5acfdb7feed570ba0cbdadc7edbb
Shiva Sharma <ss12@umbc.edu>
2023-11-20 13:12:16 -0500

    added files

---
+++ form/process-form.php
@@ -0,0 +1,33 @@
+
+<?php
+var_dump($_POST);
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+$conn = new mysqli('db', 'lamp_demo','password','lamp_demo');
+
+
+//$db = mysql_select_db($connection,"blog"); // Selecting Database from Server
+// Fetching variables of the form which travels in URL
+
+  $title= $_POST["title"];
+  $blogcontent= $_POST["blogcontent"];
+  $dateblog = date("Y-m-d");
+
+//Insert Query of SQL
+
+$stmt = $conn->prepare("INSERT INTO blog (title, body, date_created) VALUES (?, ?, ?)");
+        $stmt->bind_param("sss", $title, $blogcontent, $dateblog);
+
+        if ($stmt->execute()) {
+          echo "Data inserted successfully";
+      } else {
+          echo "Error: " . $stmt->error;
+      }
+
+      $stmt->close();
+      $conn->close();
+  
+}
+
+
+?>
---
+++ index1.php
@@ -0,0 +1,23 @@
+<?php
+
+$connect = mysqli_connect(
+    'db',
+    'lamp_demo',
+    'password',
+    'lamp_demo'
+);
+
+$query = 'SELECT * FROM blog';
+$result = mysqli_query($connect, $query);
+
+echo '<h1>MySQL Content:</h1>';
+
+while($record = mysqli_fetch_assoc($result))
+{
+    echo '<h2>'.$record['title'].'</h2>';
+    echo '<p>'.$record['body'].'</p>';
+    echo 'Posted: '.$record['date_created'];
+    echo '<hr>';
+}
+
+?>
